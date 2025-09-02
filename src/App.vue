2b0e513ea63fdbd020db405b046329c14af1350e
afsdari0 <dariorbneto2@hotmail.com>
2025-09-02 09:57:10 -0300

    Remake 0.01

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-container class="lala d-flex ">
+      <v-container class="lele">
+        <h1>A Simone</h1>
+      </v-container>
+      <v-container class="lele">
+        <h1>lolo</h1>
+      </v-container>
+    </v-container>
+  </v-app>
+</template>
+
+<style>
+
+.lala{
+  border: 1px red solid;
+  max-width: 1920px !important;
+  min-height: 100vh;
+
+}
+
+.lele{
+  border: 1px blue solid;
+
+}
+</style>
+<script setup>
+  //
+</script>
